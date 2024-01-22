2f747ff48745df656e54912919833d2e5eca3800
petrostrak <pit.trak@gmail.com>
2024-01-22 17:50:51 +0200

    Add distance calculation logic

--- distance_calculator/service.go
+++ distance_calculator/service.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"math"
 
 	"github.com/petrostrak/freight-mileage-toll-calculation-system/obu/types"
 )
@@ -10,13 +10,26 @@ type Calculator interface {
 	CalculateDistance(types.OBUData) (float64, error)
 }
 
-type CalculatorService struct{}
+type CalculatorService struct {
+	points [][]float64
+}
 
 func NewCalculatorService() *CalculatorService {
-	return &CalculatorService{}
+	return &CalculatorService{
+		points: make([][]float64, 0),
+	}
 }
 
 func (c *CalculatorService) CalculateDistance(data types.OBUData) (float64, error) {
-	fmt.Println("Calculating distance")
-	return 00, nil
+	distance := 0.0
+	if len(c.points) > 0 {
+		prevPoints := c.points[len(c.points)-1]
+		distance = calculateDistance(prevPoints[0], prevPoints[1], data.Lat, data.Long)
+	}
+	c.points = append(c.points, []float64{data.Lat, data.Long})
+	return distance, nil
+}
+
+func calculateDistance(x1, y1, x2, y2 float64) float64 {
+	return math.Sqrt(math.Pow(x2-x1, 2) + math.Pow(y2-y1, 2))
 }
