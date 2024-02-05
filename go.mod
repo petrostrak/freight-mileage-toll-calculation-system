55247d1eb29f6c359b87fcce0fb12173f0d22e86
petrostrak <pit.trak@gmail.com>
2024-02-05 17:58:32 +0200

    Add NewHTTPClient

--- go.mod
+++ go.mod
@@ -5,23 +5,28 @@ go 1.21.4
 require (
 	github.com/confluentinc/confluent-kafka-go/v2 v2.3.0
 	github.com/go-kit/kit v0.13.0
+	github.com/go-kit/log v0.2.1
 	github.com/gorilla/websocket v1.5.1
 	github.com/prometheus/client_golang v1.18.0
 	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11
 	google.golang.org/grpc v1.54.0
 	google.golang.org/protobuf v1.31.0
 )
 
 require (
+	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.45.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/smartystreets/goconvey v1.8.1 // indirect
+	github.com/sony/gobreaker v0.4.1 // indirect
+	github.com/streadway/handy v0.0.0-20200128134331-0f66f006fb2e // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
--- kit/aggsvc/aggtransport/http.go
+++ kit/aggsvc/aggtransport/http.go
@@ -1,18 +1,85 @@
 package aggtransport
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"strings"
+	"time"
 
 	"net/http"
+	"net/url"
 
+	"github.com/go-kit/kit/circuitbreaker"
 	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/ratelimit"
 	"github.com/go-kit/kit/transport"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/go-kit/log"
 	"github.com/petrostrak/freight-mileage-toll-calculation-system/kit/aggsvc/aggendpoint"
+	"github.com/petrostrak/freight-mileage-toll-calculation-system/kit/aggsvc/aggservice"
+	"github.com/sony/gobreaker"
+	"golang.org/x/time/rate"
 )
+
+func NewHTTPClient(instance string, logger log.Logger) (aggservice.Service, error) {
+	if !strings.HasPrefix(instance, "http") {
+		instance = "http://" + instance
+	}
+
+	u, err := url.Parse(instance)
+	if err != nil {
+		return nil, err
+	}
+
+	limiter := ratelimit.NewErroringLimiter(rate.NewLimiter(rate.Every(time.Second), 100))
+
+	var options []httptransport.ClientOption
+	var aggregateEndpoint endpoint.Endpoint
+	{
+		aggregateEndpoint = httptransport.NewClient(
+			"POST",
+			copyURL(u, "/aggregate"),
+			encodeHTTPGenericRequest,
+			decodeHTTPAggregateResponse,
+			options...,
+		).Endpoint()
+		aggregateEndpoint = limiter(aggregateEndpoint)
+		aggregateEndpoint = circuitbreaker.Gobreaker(gobreaker.NewCircuitBreaker(gobreaker.Settings{
+			Name:    "Aggregate",
+			Timeout: 30 * time.Second,
+		}))(aggregateEndpoint)
+	}
+
+	var calculateEndpoint endpoint.Endpoint
+	{
+		calculateEndpoint = httptransport.NewClient(
+			"POST",
+			copyURL(u, "/calculate"),
+			encodeHTTPGenericRequest,
+			decodeHTTPCalculateResponse,
+			options...,
+		).Endpoint()
+		calculateEndpoint = limiter(calculateEndpoint)
+		calculateEndpoint = circuitbreaker.Gobreaker(gobreaker.NewCircuitBreaker(gobreaker.Settings{
+			Name:    "Aggregate",
+			Timeout: 30 * time.Second,
+		}))(calculateEndpoint)
+	}
+
+	return aggendpoint.Set{
+		AggregateEndpoint: aggregateEndpoint,
+		CalculateEndpoint: calculateEndpoint,
+	}, nil
+}
+
+func copyURL(base *url.URL, path string) *url.URL {
+	next := *base
+	next.Path = path
+	return &next
+}
 
 func errorEncoder(ctx context.Context, err error, w http.ResponseWriter) {}
 
@@ -52,6 +119,16 @@ func encodeHTTPGenericResponse(ctx context.Context, w http.ResponseWriter, respo
 	}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	return json.NewEncoder(w).Encode(response)
+}
+
+func encodeHTTPGenericRequest(_ context.Context, r *http.Request, response any) error {
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(r); err != nil {
+		return err
+	}
+
+	r.Body = ioutil.NopCloser(&buf)
+	return nil
 }
 
 func NewHTTPHandler(endpoints aggendpoint.Set, logger log.Logger) http.Handler {
